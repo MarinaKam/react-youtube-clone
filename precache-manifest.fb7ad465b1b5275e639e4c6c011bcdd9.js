64d4613b374b984616f530aaa188a9262a8ab403
Marina Kam <kam.marvic@gmail.com>
2019-02-07 17:52:02 +0200

    Updates

---
+++ precache-manifest.fb7ad465b1b5275e639e4c6c011bcdd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "43d84a9d90d89dececc4",
+    "url": "/react-youtube-clone/static/js/runtime~main.43d84a9d.js"
+  },
+  {
+    "revision": "0f6315bb621a35fba0a8",
+    "url": "/react-youtube-clone/static/js/main.0f6315bb.chunk.js"
+  },
+  {
+    "revision": "003711fae1e6a186d567",
+    "url": "/react-youtube-clone/static/js/1.003711fa.chunk.js"
+  },
+  {
+    "revision": "0f6315bb621a35fba0a8",
+    "url": "/react-youtube-clone/static/css/main.23c454aa.chunk.css"
+  },
+  {
+    "revision": "dffc74d1aa3984c3e5eacdaa45ec18f1",
+    "url": "/react-youtube-clone/index.html"
+  }
+];
